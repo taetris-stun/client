462a0b75327a7bdffd5b23b012c33536c5dc640a
tinkertoe <40361609+tinkertoe@users.noreply.github.com>
2021-06-10 00:54:30 +0200

    PLAYABLE :D

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,8 @@ require('./blockrain/blockrain.jquery.src.js')
 require('./blockrain/blockrain.jquery.themes.js')
 require('./blockrain/blockrain.css')
 
-const downScaleFactor = 16
-const dropedFrames = 20
+const scaleFactor = 4
+const dropedFrames = 3
 const canvasWidth = 300
 const canvasHeight = 600
 
@@ -96,70 +96,98 @@ function setupReadyCheck(room: Colyseus.Room) {
 }
 
 function setupGame(room: Colyseus.Room) {
-  // Change view
+  room.state.players.forEach(player => {
+    // Differenciate between you and others
+    if (room.sessionId === player.sessionId) {
+      /*
+        THIS EXECUTES FOR YOURSELF
+      */
+
+      // Create html layout for local player
+      $('#game').append(`
+        <div class="player" id="${player.sessionId}-player">
+          <div class="username" id="${player.sessionId}-username"></div>
+          <div class="canvas" id="${player.sessionId}-canvas" style="width: ${canvasWidth}px; height: ${canvasHeight}px;"></div>
+        </div>
+      `)
+
+      // Set username in layout
+      $('#' + player.sessionId + '-username').text(player.username)
+
+      // Setup blockrain
+      const canvas = $('#' + player.sessionId + '-canvas')
+
+      let renderCount = 0
+      canvas.blockrain({
+        theme: 'candy',
+        onLine: (lines: number, scoreIncrement: number, score: number) => {
+          room.send('line', lines)
+        },
+        onGameOver: (score: number) => {
+          room.send('gameover', score)
+        },
+        onRender: (ctx: CanvasRenderingContext2D) => {
+          renderCount++
+          if (renderCount === dropedFrames) {
+            sendCanvas(room, ctx)
+            renderCount = 0
+          }
+        },
+      })
+
+      canvas.blockrain('start')
+
+    } else {
+      /*
+        THIS EXECUTES FOR EVERYBODY ELSE
+      */
+
+      // Create html layout for other players
+      $('#game').append(`
+        <div class="player" id="${player.sessionId}-player">
+          <div class="username" id="${player.sessionId}-username"></div>
+          <canvas class="canvas" id="${player.sessionId}-canvas" width="${canvasWidth}" height="${canvasHeight}"></canvas>
+        </div>
+      `)
+
+      // Set username in layout
+      $('#' + player.sessionId + '-username').text(player.username)
+
+      // Handle state updates
+      player.onChange = (changes) => {
+        changes.forEach(change => {
+          switch (change.field) {
+            case 'canvas': receiveCanvas(player.sessionId, change.value)
+            default: void(0)
+          }
+        })
+      }
+
+    }
+  })
+
+  // Change view after DOM is populated
   $('#config').hide()
   $('#ready').hide()
   $('#game').show()
 
-  const game = $('#blockrain')
-
-  // Set canvas dimensions
-  game.attr('style', `width: ${canvasWidth}px; height: ${canvasHeight}px;`)
-
-  let renders = 0
-  game.blockrain({
-    theme: 'candy',
-    onLine: (lines: number, scoreIncrement: number, score: number) => {
-      console.log('line', lines)
-    },
-    onGameOver: (score: number) => {
-      console.log('gameover', score)
-    },
-    onRender: (ctx: CanvasRenderingContext2D) => {
-      renders++
-      if (renders === dropedFrames) {
-        sendCanvas(room, ctx)
-        renders = 0
-      }
-    },
-  })
-
-  game.blockrain('start')
-
-
-  room.state.players.forEach(player => {
-    if (room.sessionId !== player.sessionId) {
-      // This executes for everyone but yourself
-
-      // player.onChange = (changes) => {
-      //   changes.forEach(change => {
-      //     console.log(player.username, change.field, change.value)
-      //   })
-      // }
-
-      // Add html layout for every player 
-      $('#game').append(`
-      <div class="player">
-        <div>${player.username}</div>
-        <canvas id="${player.sessionId}"></canvas>
-      </div>
-      `)
-    }
+  // To reset the client => reload the page
+  room.onMessage('reset', () => {
+    window.location.reload()
   })
 }
 
 function sendCanvas(room: Colyseus.Room, ctx: CanvasRenderingContext2D) {
-  let originalImage = Array.from(ctx.getImageData(0, 0, canvasWidth, canvasHeight).data)
-  console.log('org', originalImage.length)
-
   /*
   Downscale image data
     Loop runs for every (downScaleFactor)th item in array (every 2nd pixel)
     Calculates average of R, G and B
     Pushes B&W value into new array
   */
-  let compressedImage: number[] = []
-  for (let i = 0; i < originalImage.length; i = i+4*downScaleFactor) {
+
+  const originalImage = Array.from(ctx.getImageData(0, 0, canvasWidth, canvasHeight).data)
+  const compressedImage: number[] = []
+  for (let i = 0; i < originalImage.length; i = i+4*scaleFactor) {
     const R = originalImage[i]
     const G = originalImage[i+1]
     const B = originalImage[i+2]
@@ -169,11 +197,27 @@ function sendCanvas(room: Colyseus.Room, ctx: CanvasRenderingContext2D) {
     compressedImage.push(BW)
   }
 
-  console.log('comp', compressedImage.length)
-
   room.send('canvas', JSON.stringify(compressedImage))
+}
 
-  // Free memory (prob. useless?)
-  originalImage = []
-  compressedImage = []
+function receiveCanvas(sessionId: string, data: string) {
+  /*
+    Upscale image data
+
+  */
+  const originalImage: number[] = []
+  const compressedImage = JSON.parse(data) as Array<number>
+  for (let i1 = 0; i1 < compressedImage.length; i1++) {
+    for (let i2 = 0; i2 < scaleFactor; i2++) {
+      originalImage.push(compressedImage[i1]) // R
+      originalImage.push(compressedImage[i1]) // G
+      originalImage.push(compressedImage[i1]) // B
+      originalImage.push(255) // A
+    }
+  }
+
+  const imageData = new ImageData(Uint8ClampedArray.from(originalImage), canvasWidth, canvasHeight)
+  const canvas = $('#' + sessionId + '-canvas')[0] as HTMLCanvasElement
+  const ctx = canvas.getContext('2d')
+  ctx.putImageData(imageData, 0, 0)
 }
